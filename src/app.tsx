c0532c525d70fa1e8b4b27ad9866f4f562ca8b68
kylemckinley <kyle.mckinley@vntana.com>
2023-07-05 12:40:43 -0700

    Initial commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,40 +1,90 @@
 import * as React from 'react';
 import {createRoot} from 'react-dom/client';
 
-async function addSticky() {
-  const stickyNote = await miro.board.createStickyNote({
-    content: 'Hello, World!',
-  });
-
-  await miro.board.viewport.zoomTo(stickyNote);
-}
-
 const App: React.FC = () => {
-  React.useEffect(() => {
-    addSticky();
-  }, []);
+  const [url, setUrl] = React.useState("");
+  const [showUrlError, setShowUrlError] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
 
+  // URL Validation
+  function validateUrl(url: string) {
+    return /^https?:\/\/embed\.vntana\.com\?/.test(url);
+  }
+
+  const disableBtn = async (setting: boolean) => {
+    (document.getElementById("embed-button") as HTMLButtonElement).disabled = setting;
+  }
+
+  const checkUrl = async () => {
+    setShowUrlError(false);
+    if (!validateUrl(url) || !url.includes("productUuid=") || !url.includes("clientSlug=") || !url.includes("organizationSlug=")){
+      if (url.trim().length >= 1)
+        setShowUrlError(true);
+    } else {
+      setShowUrlError(false);
+    }
+  }
+
+  async function embedViewer() {
+    setShowUrlError(false);
+    setIsLoading(true);
+
+    if (!validateUrl(url) || !url.includes("productUuid=") || !url.includes("clientSlug=") || !url.includes("organizationSlug=")){
+      setShowUrlError(true);
+    } else {
+      const embedFrame = await miro.board.createEmbed({
+          url: url,
+          mode: 'modal',
+          width: 500,
+          height: 500,
+          x: 1600,
+          y: 1600
+        });
+        
+        await miro.board.viewport.zoomTo(embedFrame);
+        setUrl("");
+    }
+    setIsLoading(false);
+  }
+
+  const disabled = url.trim().length < 1;
   return (
     <div className="grid wrapper">
       <div className="cs1 ce12">
-        <img src="/src/assets/congratulations.png" alt="" />
+        <img width="300" height="37" src="https://www.vntana.com/wp-content/uploads/2020/12/VNTANA-logo-blue-1536x190.png" alt="" />
       </div>
       <div className="cs1 ce12">
-        <h1>Congratulations!</h1>
-        <p>You've just created your first Miro app!</p>
-        <p>
-          To explore more and build your own app, see the Miro Developer
-          Platform documentation.
-        </p>
+        <p>The VNTANA app will allow you to embed your VNTANA 3D assets directly into your Miro workspace!</p>
+        <p>Simply copy the share link of the asset on the VNTANA Platform that you wish to embed, and enter it below</p>
       </div>
       <div className="cs1 ce12">
-        <a
-          className="button button-primary"
-          target="_blank"
-          href="https://developers.miro.com"
-        >
-          Read the documentation
-        </a>
+        <div className={`form-group ${showUrlError ? 'error' : ''}`} id="embed-group">
+          <label htmlFor="embed-input">Add 3D Share Link</label>
+          <input 
+            className="input"
+            id="embed-input"
+            type="text"
+            value={url}
+            onChange={(event)=> {
+              setUrl(event.target.value);
+              if (url.trim().length >= 1)
+                disableBtn(false);
+            }}
+            onBlur={()=> {checkUrl();}}
+          />
+          { showUrlError ? 
+            <div className="status-text" id="embed-group-error">"Error, invalid share link!"</div> : null }
+        </div>
+      </div>
+      <div className="cs1 ce12">
+        <button 
+          className={`button button-primary ${isLoading ? 'button-loading' : ''}`}
+          id="embed-button"
+          onClick={() => {
+            embedViewer();
+          }}
+          disabled={disabled}
+        >Embed 3D</button>
       </div>
     </div>
   );
